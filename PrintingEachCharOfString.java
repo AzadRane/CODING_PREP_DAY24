0f89042e444121ba44cbcbffb55f6931972a5ffb
Harish Rane <raneharish11@gmail.com>
2023-05-21 22:07:24 +0530

    print all the characters of the string

---
+++ PrintingEachCharOfString.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class PrintingEachCharOfString 
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the string");
+        String s = sc.nextLine();
+
+        for(int i=0;i<s.length();i++)
+        {
+            System.out.println(s.charAt(i));
+        }
+
+        sc.close();
+    }
+}
